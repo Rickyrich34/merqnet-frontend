81b7fc7e0ab0977e6923d51fca587253fedcb30d
Ricardo Ramirez Toro <Barman34343@gmail.com>
2026-01-21 15:34:07 -0400

    debug: nav mount test

--- src/App.jsx
+++ src/App.jsx
@@ -1,92 +1,113 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+/* PAGES */
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
 import MainDashboard from "./pages/MainDashboard";
-import Messages from "./pages/Messages";
-import History from "./pages/History";
-
-import ProfileView from "./pages/ProfileView";
-import EditProfile from "./pages/EditProfile";
-
-import PaymentMethods from "./pages/PaymentMethods";
-import ReceiptView from "./pages/ReceiptView";
-
-import About from "./pages/About";
-import Help from "./pages/Help";
-import Privacy from "./pages/Privacy";
-import Terms from "./pages/Terms";
-
 import BuyerDashboard from "./pages/BuyerDashboard";
 import SellerDashboard from "./pages/SellerDashboard";
 
 import CreateRequest from "./pages/CreateRequest";
 import SubmitBid from "./pages/SubmitBid";
-import SellerBids from "./pages/SellerBids";
 import AcceptBid from "./pages/AcceptBid";
 
-import AskTheSeller from "./pages/AskTheSeller";
-import BuyerThread from "./pages/BuyerThread";
 import AnswerBack from "./pages/AnswerBack";
 import SellerAnswer from "./pages/SellerAnswer";
+import AskTheSeller from "./pages/AskTheSeller";
+import BuyerThread from "./pages/BuyerThread";
 
+import SellerProducts from "./pages/SellerProducts";
+import SellerBids from "./pages/SellerBids";
+
+import PaymentMethods from "./pages/PaymentMethods";
+
+import ProfileView from "./pages/ProfileView";
+import EditProfile from "./pages/EditProfile";
 import ChangePassword from "./pages/ChangePassword";
 
-const App = () => {
+import ReceiptView from "./pages/ReceiptView";
+
+import Help from "./pages/Help";
+import About from "./pages/About";
+import Privacy from "./pages/Privacy";
+import Terms from "./pages/Terms";
+import History from "./pages/History";
+import Messages from "./pages/Messages";
+
+/* COMPONENTS */
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import InactivityLogout from "./components/InactivityLogout";
+
+function App() {
   return (
     <Router>
+      <InactivityLogout timeout={15 * 60 * 1000} />
+      <div style={{color:"lime",position:"fixed",top:0,left:0,zIndex:999999}}>
+  NAV TEST
+</div>
+
+      <Navbar />
+
       <Routes>
-        {/* Public */}
+        {/* AUTH */}
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
 
-        {/* Core */}
+        {/* DASHBOARDS */}
         <Route path="/dashboard" element={<MainDashboard />} />
-        <Route path="/messages" element={<Messages />} />
-        <Route path="/history" element={<History />} />
-
-        {/* Profile */}
-        <Route path="/profile" element={<ProfileView />} />
-        <Route path="/editprofile" element={<EditProfile />} />
-
-        {/* IMPORTANT: Settings.jsx does NOT exist -> map /settings to EditProfile */}
-        <Route path="/settings" element={<EditProfile />} />
-
-        {/* Payments / Receipts */}
-        <Route path="/paymentmethods" element={<PaymentMethods />} />
-        <Route path="/receipt/:receiptId" element={<ReceiptView />} />
-
-        {/* Info */}
-        <Route path="/about" element={<About />} />
-        <Route path="/help" element={<Help />} />
-        <Route path="/privacy" element={<Privacy />} />
-        <Route path="/terms" element={<Terms />} />
-
-        {/* Buyer/Seller */}
         <Route path="/buyer-dashboard" element={<BuyerDashboard />} />
         <Route path="/seller-dashboard" element={<SellerDashboard />} />
 
-        {/* Requests/Bids */}
-        <Route path="/createrequest" element={<CreateRequest />} />
-        <Route path="/submitbid/:requestId" element={<SubmitBid />} />
-        <Route path="/sellerbids" element={<SellerBids />} />
-        <Route path="/acceptbid/:bidId" element={<AcceptBid />} />
+        {/* REQUESTS / BIDS FLOW */}
+        <Route path="/create-request" element={<CreateRequest />} />
+        <Route path="/submit-bid/:requestId" element={<SubmitBid />} />
+        <Route path="/accept-bid/:bidId" element={<AcceptBid />} />
 
-        {/* Threads / Q&A */}
-        <Route path="/asktheseller/:bidId" element={<AskTheSeller />} />
-        <Route path="/buyerthread/:threadId" element={<BuyerThread />} />
-        <Route path="/answerback/:threadId" element={<AnswerBack />} />
-        <Route path="/selleranswer/:threadId" element={<SellerAnswer />} />
+        {/* MESSAGE THREADS / Q&A */}
+        <Route path="/answer-back/:threadId" element={<AnswerBack />} />
+        <Route path="/seller-answer/:threadId" element={<SellerAnswer />} />
+        <Route path="/ask-the-seller/:requestId" element={<AskTheSeller />} />
+        <Route path="/buyer-thread/:threadId" element={<BuyerThread />} />
 
-        {/* Account */}
+        {/* SELLER */}
+        <Route path="/seller-products" element={<SellerProducts />} />
+        <Route path="/seller-bids" element={<SellerBids />} />
+
+        {/* PAYMENTS */}
+        <Route path="/payment-methods" element={<PaymentMethods />} />
+
+        {/* PROFILE */}
+        <Route path="/profile" element={<ProfileView />} />
+        <Route path="/editprofile" element={<EditProfile />} />
+
+        {/* FIX: map /settings to EditProfile */}
+        <Route path="/settings" element={<EditProfile />} />
+
         <Route path="/changepassword" element={<ChangePassword />} />
+
+        {/* OTHER */}
+        <Route path="/receipt/:receiptId" element={<ReceiptView />} />
+        <Route path="/help" element={<Help />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/history" element={<History />} />
+
+        {/* MESSAGES */}
+        <Route path="/messages" element={<Messages />} />
+
+        {/* fallback */}
+        <Route path="*" element={<Home />} />
       </Routes>
+
+      <Footer />
     </Router>
   );
-};
+}
 
 export default App;
